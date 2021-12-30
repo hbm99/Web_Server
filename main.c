20e3c51de4a499b3c9de34d56739e206d096ac12
hbm99 <you@example.com>
2021-12-29 19:23:55 -0500

    Init commit

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  web_server
+//
+//  Created by Hansel Blanco on 29/12/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
